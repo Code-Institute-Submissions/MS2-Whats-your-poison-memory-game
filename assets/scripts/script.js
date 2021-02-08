2d49763e8f6ddbe7bb633e6f70c7568d6a01609a
David-A-Ray <Mr.David.A.Ray@gmail.com>
2021-02-08 15:35:30 +0000

    card flip class added and first JS function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,46 +1,8 @@
 
-const cardImages = ['aviation', 'bloodyMary', 'champagneCocktail', 'cosmopolitan', 'french75', 'longIsland', 'maiTai', 
-'margarita', 'martini', 'maryPickford', 'mimosa', 'mojito', 'oldFashioned', 'piscoSour', 'tequilaSunrise'];
-let cardsLength = 0;
-let cardsPerRow;
-let gameLevel = sessionStorage.getItem("gameLevel");
-let matchedPairs = 0;
-let maxPairs = 0;
+const cards = document.querySelectorAll('.cards');
 
-// Check local and session storage
-checkDataStorage();
+function flipCard() {
+    this.classList.toggle('flip');
+}
 
-// Check game difficulty setting
-gameSetup();
-
-// Seting game difficulty in local storage
-$('#easy').click(function () {
-    sessionStorage.setItem("gameLevel", "easy");
-});
-$('#medium').click(function () {
-    sessionStorage.setItem("gameLevel", "medium");
-});
-$('#hard').click(function () {
-    sessionStorage.setItem("gameLevel", "hard");
-});
-
-// Game set up to start
-function gameSetup() {
-    switch (gameLevel) {
-        case ("easy"):
-            maxPairs = 6;
-            cardsLength = 12;
-            cardsPerRow = 'col-3';
-            break;
-        case ("medium"):
-            maxPairs = 10;
-            cardsLength = 20;
-            cardsPerRow = 'col-3';
-            break;
-        case ("hard"):
-            maxPairs = 15;
-            cardsLength = 30;
-            cardsPerRow = 'col-2';
-            break;
-    }
-};
+cards.forEach(card => card.addEventListener('click', flipCard))
